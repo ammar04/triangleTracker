5717ad34d7845e4f7b30fc3578082c6ba501c90a
Your Name <you@example.com>
2018-08-10 18:58:15 +0300

    fixed spacing and indentation

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,15 @@ function triangle(){
 	tracker.push(parseInt(prompt("Enter the first number of the triangle: ")));
 		if(isNaN(tracker[0]) === true || tracker[0] <= 0){
 			alert("Please enter a valid number");
-		}else{
+		}else {
 			tracker.push(parseInt(prompt("Enter the second number of the triangle:")));
 				if(isNaN(tracker[1]) === true || tracker[1] <= 0){
 					alert("Please enter a valid number");
-				}else{
+				}else {
 					tracker.push(parseInt(prompt("Enter the third number of the triangle:")));
 					if(isNaN(tracker[2]) === true || tracker[2] <= 0){
 						alert("Please enter a valid number");
-					}else{
+					}else {
 						if((tracker[0] + tracker[1]) <= tracker[2] || (tracker[1] + tracker[2]) <= tracker[0] || (tracker[0] + tracker[2]) <= tracker[1]){
 							alert("It's NOT A triangle");
 						}
@@ -21,7 +21,7 @@ function triangle(){
 							}
 							else if(tracker[0] === tracker[1] || tracker[1] === tracker[2] || tracker[0] === tracker[2]){
 								alert("It's an isosceles triangle");
-							}else{
+							}else {
 								alert("It's a scarlene triangle");
 							}
 					}
